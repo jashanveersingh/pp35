7e300bb05b9fab3aa578921d58058620ff345a19
jashanveersingh <78601252+jashanveersingh@users.noreply.github.com>
2021-05-25 18:32:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var dog,dogImage,doghappy;
+var food;
+var database;
+var foodS,foodStock;
+
+function preload(){
+
+dogImage=loadImage("images/dogImg.png");
+doghappy=loadImage("images/dogImg1.png");
+
+
+	
+}
+
+function setup() {
+	createCanvas(500,500);
+   database=firebase.database() ;
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogImage) ;
+  dog.scale=0.15;
+  foodStock=database.ref(' Food');
+  foodStock.on("value",readStock);
+  textSize(20)
+
+
+
+}
+
+
+function draw() {  
+ 
+  background("pink");
+ if(keyWentDown(UP_ARROW)) {
+  
+  writeStock(foodS);
+  dog.addImage(doghappy);
+
+ }
+
+
+
+
+
+
+  drawSprites();
+  text("foodremaing"+foodS,170,200)
+  textSize(13);
+  text("note:prees up-arrow to feed milk",130,10,300,20);
+  
+
+}
+
+function readStock(data){
+foodS=data.val()
+
+}
+function writeStock(x){
+if(x<=0){
+  x=0
+}else{
+  x=x-1
+}
+database.ref("/").update({
+Food:x
+
+})
+}
